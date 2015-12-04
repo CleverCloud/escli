f6343ea0ce590a04c1475ea25fdbf61b328dccec
Quentin ADAM <quentin.adam@waxzce.org>
2015-12-04 21:04:16 +0100

    fix autocomplete

--- src/complete.js
+++ src/complete.js
@@ -1,16 +1,16 @@
 var _ = require("lodash");
+var cliparse = require("cliparse");
 
 
 module.exports = function(config, es_request) {
 
   return {
     nodes_complete: function() {
-      console.log("ffff");
       return es_request.r({
         dpath: '_nodes'
       }).then(function(nodes_res) {
         var data = JSON.parse(nodes_res);
-        return _.pluck(data.nodes, 'name');
+        return cliparse.autocomplete.words(_.pluck(data.nodes, 'name'));
       });
     }
 
